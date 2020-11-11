52c0886bc9ce23434516423cb423f06a8ab6d1c2
thayron <thayronf3@gmail.com>
2020-11-11 00:01:01 -0300

    Criando as rotas para os episodios

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,18 @@ $router->get('/', function () use ($router) {
 });
 
 
-$router->group(['prefix' => '/api'], function () use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('series', 'SeriesController@store');
     $router->get('series', 'SeriesController@index');
     $router->get('series/{id}', 'SeriesController@show');
     $router->put('series/{id}', 'SeriesController@update');
     $router->delete('series/{id}', 'SeriesController@destroy');
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->post('episodios', 'EpisodiosController@store');
+    $router->get('episodios', 'EpisodiosController@index');
+    $router->get('episodios/{id}', 'EpisodiosController@show');
+    $router->put('episodios/{id}', 'EpisodiosController@update');
+    $router->delete('episodios/{id}', 'EpisodiosController@destroy');
 });
